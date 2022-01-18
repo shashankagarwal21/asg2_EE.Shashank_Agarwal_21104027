d5e5827911c5f24b7eb80139572946018b496cd9
shashankagarwal21 <97078764+shashankagarwal21@users.noreply.github.com>
2022-01-18 17:51:25 +0530

    Create Assignment_3.py

---
+++ Assignment_3/Assignment_3.py
@@ -0,0 +1,13 @@
+a = 56  # Declare value of a from question
+b = 10  # Declare value of b from question
+
+print("a is " + str(a))  # print value of a
+print("b is " + str(b) + "\n")  # print value of b
+
+print("a & b = " + str(a & b))  # Calculate bitwise and, and print it
+print("a | b = " + str(a | b))  # Calculate bitwise or, and print it
+print("a ^ b = " + str(a ^ b))  # Calculate bitwise xor, and print it
+print("a left shift by 2 bit is " + str(a << 2))  # left shift a by 2 bit and print it
+print("b left shift by 2 bit is " + str(b << 2))  # left shift b by 2 bit and print it
+print("a left shift by 2 bit is " + str(a >> 2))  # right shift a by 2 bit and print it
+print("b left shift by 2 bit is " + str(b >> 2))  # right shift b by 2 bit and print it
