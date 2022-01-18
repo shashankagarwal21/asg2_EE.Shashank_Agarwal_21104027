0b82d0d779c7a677bae2149c8fccef0ac889d962
shashankagarwal21 <97078764+shashankagarwal21@users.noreply.github.com>
2022-01-18 17:50:31 +0530

    Create Assignment_2.py

---
+++ Assignment_2/Assignment_2.py
@@ -0,0 +1,8 @@
+name = input("Enter your name ")  # take name as input from user
+sid = int((input("Enter your SID ")))  # take SID as input from user
+depar = input("Enter your department ")  # take Department as input from user
+cgpa = float(input("Enter your CGPA "))  # take CGPA as input from user
+
+print("Hey, " + name + " Here!")  # print the name
+print("My SID is " + str(sid))  # print the SID
+print("I am from " + depar + " and my CGPA is " + str(cgpa))  # print the Department and CGPA
