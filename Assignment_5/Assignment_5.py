640cc5609bcaba3f53f65232ff576f42d7a01c16
shashankagarwal21 <97078764+shashankagarwal21@users.noreply.github.com>
2022-01-18 17:53:33 +0530

    Create Assignment_5.py

---
+++ Assignment_5/Assignment_5.py
@@ -0,0 +1,6 @@
+string = input("Enter a String ")  # take a string input form user
+
+if "name" in string:  # check if name is present in the string
+    print("Yes")      # if yes print "Yes"
+else:
+    print("No")       # else print "No"
